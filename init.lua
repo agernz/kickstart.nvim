b38ea25c13d926196062cf57055459d1b1791eaa
agernz <agernz@gmail.com>
2025-04-18 18:18:07 -0500

    feat: add obsidian keybinds (new, search)

--- init.lua
+++ init.lua
@@ -119,6 +119,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Obsidian keymaps
+vim.keymap.set('n', '<leader>os', '<cmd>ObsidianSearch<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>ot', '<cmd>ObsidianNewFromTemplate<CR>', { noremap = true, silent = true })
+
 -- AutoCmds --{{{
 
 vim.api.nvim_create_autocmd('FileType', {
